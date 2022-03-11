d99b1fa3cd527c4536d132f207300ff8fba8eab4
kangwsh <kangwsh22@163.com>
2022-03-11 21:30:29 +0800

    usage of const *

---
+++ heima/14/test1.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+void test01(){
+	const int &i = 10;
+
+	int *p = (int *)&i;
+	*p = 1000;
+
+	cout << "i = " << i << endl;
+}
+
+void test02(){
+	const int i = 10;
+
+	int *p = (int *)&i;
+	*p = 1000;
+
+	cout << "test2 i = " << i << endl;
+}
+
+void test03(){
+	const int i = 10;
+
+	const int &ref = i;
+	int *p = (int *)&ref;
+	*p = 1000;
+
+	cout << "test3 i = " << i << endl;
+}
+
+void test04(){
+	const int &i = 10;
+	int *p = &i;
+	*p = 1000;
+	cout << "test4 i = " << i << endl;
+}
+void test05(){
+	const int i = 10;
+	const int *p = &i;
+	*p = 1000;
+	cout << "test5 i = " << i << endl;
+}
+void test06(){
+	const int i = 10;
+	int *p = &i;
+	*p = 1000;
+	cout << "test6 i = " << i << endl;
+}
+int main(){
+	test01();
+	test02();
+	test03();
+	test04();
+	test05();
+	test06();
+	return 0;
+}
