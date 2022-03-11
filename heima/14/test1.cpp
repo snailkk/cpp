17aed00dcf4a930ce46ccc5298e4d2ed970060e5
kangwsh <kangwsh22@163.com>
2022-03-11 21:47:42 +0800

    usage of const

--- heima/14/test1.cpp
+++ heima/14/test1.cpp
@@ -24,34 +24,35 @@ void test03(){
 	const int &ref = i;
 	int *p = (int *)&ref;
 	*p = 1000;
+	cout << "test3 ref = " << ref << endl;
 
 	cout << "test3 i = " << i << endl;
 }
-
-void test04(){
-	const int &i = 10;
-	int *p = &i;
-	*p = 1000;
-	cout << "test4 i = " << i << endl;
-}
-void test05(){
-	const int i = 10;
-	const int *p = &i;
-	*p = 1000;
-	cout << "test5 i = " << i << endl;
-}
-void test06(){
-	const int i = 10;
-	int *p = &i;
-	*p = 1000;
-	cout << "test6 i = " << i << endl;
-}
+//
+//void test04(){ //error
+//	const int &i = 10;
+//	int *p = &i;
+//	*p = 1000;
+//	cout << "test4 i = " << i << endl;
+//}
+//void test05(){ //error
+//	const int i = 10;
+//	const int *p = &i;
+//	*p = 1000;
+//	cout << "test5 i = " << i << endl;
+//}
+//void test06(){ //error
+//	const int i = 10;
+//	int *p = &i;
+//	*p = 1000;
+//	cout << "test6 i = " << i << endl;
+//}
 int main(){
 	test01();
 	test02();
 	test03();
-	test04();
-	test05();
-	test06();
+	//test04();
+	//test05();
+	//test06();
 	return 0;
 }
