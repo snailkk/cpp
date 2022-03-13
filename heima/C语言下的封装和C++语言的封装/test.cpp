9ac3146d1175e5d0c08e4732e7d51cca4735d3a9
kangwsh <kangwsh22@163.com>
2022-03-13 19:34:16 +0800

    删除闲杂文件

---
+++ heima/C语言下的封装和C++语言的封装/test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+//C++中的封装 严格类型转换检测，让属性和行为绑定到一起
+//属性和行为作为一个整体来表示生活中的事物
+//public 在类内部和类外部都可以访问
+//protected 类内部，和当前类的子类可以访问
+//private 在类内部可以访问，类外部不可以访问  
